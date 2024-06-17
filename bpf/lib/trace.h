854dbc7c206a4041de68db2331a6c39febdffe27
Ben Bigdelle <bigdelle@google.com>
2024-06-17 23:06:07 +0000

    Adding ability to read trace_id from data_meta

--- bpf/lib/trace.h
+++ bpf/lib/trace.h
@@ -199,10 +199,17 @@ _send_trace_notify(struct __ctx_buff *ctx, enum trace_point obs_point,
 		   __u32 src, __u32 dst, __u16 dst_id, __u32 ifindex,
 		   enum trace_reason reason, __u32 monitor, __u16 line, __u8 file)
 {
+    void *data_meta = (void *)(long)ctx->data_meta;
+    void *data = (void *)(long)ctx->data;
+	__u32 trace_id;
 	__u64 ctx_len = ctx_full_len(ctx);
 	__u64 cap_len = min_t(__u64, monitor ? : TRACE_PAYLOAD_LEN,
 			      ctx_len);
 	struct trace_notify msg __align_stack_8;
+
+	if ((data_meta + sizeof(__u32)) <= data){
+		trace_id = *(__u32 *)data_meta;
+	}
 
 	_update_trace_metrics(ctx, obs_point, reason, line, file);
 
@@ -217,7 +224,7 @@ _send_trace_notify(struct __ctx_buff *ctx, enum trace_point obs_point,
 		.dst_id		= dst_id,
 		.reason		= reason,
 		.ifindex	= ifindex,
-		.trace_id 	= HARDCODED_TRACE_ID,
+		.trace_id 	= trace_id,
 	};
 	memset(&msg.orig_ip6, 0, sizeof(union v6addr));
 
@@ -231,10 +238,17 @@ send_trace_notify4(struct __ctx_buff *ctx, enum trace_point obs_point,
 		   __u32 src, __u32 dst, __be32 orig_addr, __u16 dst_id,
 		   __u32 ifindex, enum trace_reason reason, __u32 monitor)
 {
+	void *data_meta = (void *)(long)ctx->data_meta;
+    void *data = (void *)(long)ctx->data;
+	__u32 trace_id;
 	__u64 ctx_len = ctx_full_len(ctx);
 	__u64 cap_len = min_t(__u64, monitor ? : TRACE_PAYLOAD_LEN,
 			      ctx_len);
 	struct trace_notify msg;
+
+	if ((data_meta + sizeof(__u32)) <= data){
+		trace_id = *(__u32 *)data_meta;
+	}
 
 	update_trace_metrics(ctx, obs_point, reason);
 
@@ -251,7 +265,7 @@ send_trace_notify4(struct __ctx_buff *ctx, enum trace_point obs_point,
 		.ifindex	= ifindex,
 		.ipv6		= 0,
 		.orig_ip4	= orig_addr,
-		.trace_id 	= HARDCODED_TRACE_ID,
+		.trace_id 	= trace_id,
 	};
 
 	ctx_event_output(ctx, &EVENTS_MAP,
