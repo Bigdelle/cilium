5ecce2f54b00f1e6d76f8298d197e16aad15d0c5
Ben Bigdelle <bigdelle@google.com>
2024-06-21 22:38:00 +0000

    Changed trace_id from 32 bits to 64 bit support

--- bpf/lib/trace_helpers.h
+++ bpf/lib/trace_helpers.h
@@ -6,19 +6,20 @@
 #include "common.h"
 #include "ip_options.h"
 
+// Define the trace ID map with __u64 trace_id
 struct {
     __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
     __uint(max_entries, 1);
     __type(key, __u32); // only one key here
-    __type(value, __u32); // trace_id type. 
-    //TODO change value to 64 bit
+    __type(value, __u64); // trace_id type
+    __uint(pinning, LIBBPF_PIN_BY_NAME);
 } trace_id_map __section_maps_btf;
 
 /* Macro to set trace_id in the map */
 #define bpf_trace_id_set(trace_id)                        \
     ({                                                    \
         __u32 __z = 0;                                    \
-        __u32 *__cache = map_lookup_elem(&trace_id_map, &__z); \
+        __u64 *__cache = map_lookup_elem(&trace_id_map, &__z); \
         if (always_succeeds(__cache))                     \
             *__cache = trace_id;                          \
         trace_id;                                         \
@@ -28,27 +29,27 @@ struct {
 #define bpf_trace_id_get()                                \
     ({                                                    \
         __u32 __z = 0;                                    \
-        __u32 *__cache = map_lookup_elem(&trace_id_map, &__z); \
-        __u32 trace_id = 0;                               \
+        __u64 *__cache = map_lookup_elem(&trace_id_map, &__z); \
+        __u64 trace_id = 0;                               \
         if (always_succeeds(__cache))                     \
             trace_id = *__cache;                          \
         trace_id;                                         \
     })
 
-static __always_inline __u32 load_trace_id() {
+static __always_inline __u64 load_trace_id() {
    return bpf_trace_id_get();
 }
 
 // Function to check trace ID and store it if valid
 static __always_inline void check_and_store_trace_id(struct __ctx_buff *ctx) {
-    __u32 trace_id = trace_id_from_ctx(ctx);
+    __u64 trace_id = trace_id_from_ctx(ctx);
 
     // Check if the trace ID is valid
-    if (trace_id > 0) {
+    if (trace_id != 0) {
         bpf_trace_id_set(trace_id);
+    } else {
+        bpf_trace_id_set(0); // Zero-out if no trace_id
     }
-    bpf_trace_id_set(trace_id); //for now still save the trace_id so we can see the parsing error
-    //TODO: zero'ing out
 }
 
 #endif // TRACE_ID_UTIL_H
